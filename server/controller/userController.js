d6f1a650ed12710c6ff28c77bbddd722ffae4516
Boris Burdeyniy <borisbutdeyniy@gmail.com>
2025-01-05 21:10:08 +0200

    user session

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,16 +1,55 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
+require("dotenv").config();
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "4h",
+  });
+};
 
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
 
-  async login(req, res) {}
+    if (!email || !password) {
+      return next(ApiError.badRequest("Incorect email or password"));
+    }
+
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiError.badRequest("User with this email exist"));
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
+
+    return res.json({ token });
+  }
+
+  async login(req, res, next) {
+    console.log("login");
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.internal("User not found"));
+    }
+
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Incorect password"));
+    }
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
 
   async check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest("Hasn't set ID"));
-    }
-    res.json(id);
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
   }
 }
 
